1cd5780c1c4c32b830cac43e53e2f13d2e367974
inDisguise810 <104oakil@gmail.com>
2025-10-31 00:58:41 +0600

    added libs

---
+++ lab1/task1.py
@@ -0,0 +1,4 @@
+from OpenGL.GL import *      # Core OpenGL functions
+from OpenGL.GLUT import *    # GLUT library for window and input handling
+from OpenGL.GLU import *     # OpenGL Utility library
+import math
